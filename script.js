0671a9d57970478c7c55d6fcf80e74c1b64af006
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-10 21:04:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://dce-ak-livedazn.akamaized.net/dashrdm/dazn-linear-056/stream.mpd";
+}
+
+function getClearKey() {
+  return "1b144f73e6fefe91cd05f850e2b589d0";
+}
+
+function getClearKeyId() {
+  return "82f8e2a17dac44c0a18f660479349c59";
+}
